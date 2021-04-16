142c286113d134840f7f4cb63bbffb0a04de1c02
Sepehr79 <68536625+Sepehr79@users.noreply.github.com>
2021-04-16 21:57:23 +0430

    pointcut declaration

--- src/main/java/spring_course/aop/before/beans/aspect/BeforeAspect.java
+++ src/main/java/spring_course/aop/before/beans/aspect/BeforeAspect.java
@@ -3,18 +3,24 @@ package spring_course.aop.before.beans.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class BeforeAspect {
 
-    /**
-     * This method will be executed before invoke 'processLogic' method
-     */
-    @Before("execution(void spring_course.aop.before.beans.Logic.process*(int, spring_course.aop.before.beans.Person, *, ..))")
+    @Pointcut("execution(void spring_course.aop.before.beans.Logic.process*(int, spring_course.aop.before.beans.Person, *, ..))")
+    public void processAspect(){}
+
+    @Before("processAspect()")
     public void beforeMethod(){
         System.out.println("Run before void advice!");
+    }
+
+    @Before("processAspect()")
+    public void beforeMethod2(){
+        System.out.println("Second run before void advice!");
     }
 
     @Before("execution(* spring_course.aop.before.beans.*.*(..))") // Matches any methods in arg package
